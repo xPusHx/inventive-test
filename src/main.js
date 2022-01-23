a0bb202b51ad0dd2c16a4f41f4e85f786aa11b90
Pavel Kharkov <push1208@gmail.com>
2022-01-23 09:09:48 +0300

    use @

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue';
-import router from './router';
-import store from './store';
+import router from '@/router';
+import store from '@/store';
 
 import {BootstrapVue, IconsPlugin} from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-import App from './App.vue';
+import App from '@/App.vue';
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
