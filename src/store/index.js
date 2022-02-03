732b8d24a03db0a569f856aa27265fb62e8bec10
Pavel Kharkov <push1208@gmail.com>
2022-02-03 13:03:39 +0300

    add ext

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,7 @@ import Vuex from 'vuex';
 import actions from './actions.js';
 import getters from './getters.js';
 import mutations from './mutations.js';
-import state from './state';
+import state from './state.js';
 
 Vue.use(Vuex);
 
