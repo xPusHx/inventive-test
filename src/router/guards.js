9e83b3f022f74861df165d8311957b8fa714b64f
Pavel Kharkov <push1208@gmail.com>
2022-02-04 19:38:41 +0300

    set page title in afterEach

--- src/router/guards.js
+++ src/router/guards.js
@@ -1,7 +1,14 @@
+import Vue from 'vue';
+
 const initRouterGuards = router => {
     router.beforeEach((routeTo, routeFrom, next) => {
-        document.title = routeTo.meta.title;
         next();
+    });
+
+    router.afterEach(routeTo => {
+        Vue.nextTick(() => {
+            document.title = routeTo.meta.title;
+        });
     });
 };
 
